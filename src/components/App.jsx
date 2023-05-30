616c379b6ac6e43874b1a827f072f7f119f200ab
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-05-30 23:02:25 +0300

    add class App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <>Task 2.2</>;
+  }
+}
+
+export default App;
