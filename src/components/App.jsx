eaa2a23555c785af5bcbcea98a87ffc87e2c8879
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-05-31 22:55:09 +0300

    add fields from HW task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,38 @@ import { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <>Task 2.2</>;
+    return (
+      <>
+        <h1>Phonebook</h1>
+
+        <form>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+
+          <label>Number</label>
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </form>
+
+        <button type="submit">Add contact</button>
+
+        <h2>Contacts</h2>
+
+        <label>Find contact by name</label>
+        <input type="text" name="filter" required />
+      </>
+    );
   }
 }
 
